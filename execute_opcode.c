3cdee0cf57b308a84dde955714cdd90543a126d0
Sana Assim <sanaassim1@gmail.com>
2023-06-21 12:15:46 -0700

    Execute a Monty bytecode instruction

---
+++ execute_opcode.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * execute_opcode - Executes a Monty bytecode instruction
+ *
+ * @instruction: The instruction to be executed
+ * @ln: The line number of the instruction
+ * @top: A double pointer to the top of the stack
+ *
+ * Return: void
+ */
+void execute_opcode(char *instruction, unsigned int ln, stack_t **top)
+{
+	char *opcode;
+	void (*f)(stack_t **, unsigned int);
+
+	opcode = get_opcode(instruction);
+	if (opcode && opcode[0] != '#')
+	{
+		f = instruction_func(opcode);
+		if (!f)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", ln, opcode);
+			exit(EXIT_FAILURE);
+		}
+
+		f(top, ln);
+	}
+}
