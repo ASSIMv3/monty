6aaedb01087cfff5ade8c668a7ebc222ed95a1b3
Sana Assim <sanaassim1@gmail.com>
2023-06-21 14:24:18 -0700

    Function that reaturn the top of the stack

---
+++ stack_top.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * stack_top - Get the top element of the stack and update the stack
+ *
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the stack_top instruction
+ *
+ * Return: Pointer to the top element of the stack
+ */
+stack_t *stack_top(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *top, *new_top;
+
+	if (struct_state == IN_STACK)
+	{
+		new_top = (*stack)->prev;
+		if (new_top)
+			new_top->next = NULL;
+	}
+	else
+	{
+		new_top = (*stack)->next;
+		if (new_top)
+			new_top->prev = NULL;
+	}
+	top = *stack;
+	*stack = new_top;
+	return (top);
+}
