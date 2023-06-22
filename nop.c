eb6f7c35992e351aef40f470804d4844957756bc
Sana Assim <sanaassim1@gmail.com>
2023-06-22 06:52:44 -0700

    Fix: update function

--- nop.c
+++ nop.c
@@ -5,8 +5,8 @@
  * @stack: Pointer to the top of the stack (unused)
  * @line_number: Line number of the opcode in the bytecode file (unused)
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(__attribute__((unused)) stack_t **stack,
+		__attribute__((unused)) unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	/* Do Nothing */
 }
