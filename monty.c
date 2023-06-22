5ac4d425cad1da24da1ee1c96cb6aec752de211b
Sana Assim <sanaassim1@gmail.com>
2023-06-22 06:54:01 -0700

    Fix: change var names

--- monty.c
+++ monty.c
@@ -1,42 +1,44 @@
 #include "monty.h"
 
 /**
- * main - Entry point for the Monty interpreter
- * @ac: Number of command-line arguments
- * @av: Array of command-line arguments
- * Return: 0 on success
+ * main - Entry point
+ *
+ * @argc: Arguments count
+ * @argv: Arguments vector
+ *
+ * Return: 0 (Success)
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int fd;
+	int file;
 	char *buffer = NULL;
 	size_t i = 0;
-	unsigned int line_number = 1;
+	unsigned int line_num = 1;
 	stack_t *top = NULL;
 
-	struct_state = IN_STACK;
-	if (ac != 2)
+	stack_state = ELEMENT_IN_STACK;
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	file = open(argv[1], O_RDONLY);
+	if (file == -1)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	while (custom_getline(&buffer, &i, fd) != -1)
+	while (custom_getline(&buffer, &i, file) != -1)
 	{
-		execute_opcode(buffer, line_number, &top);
-		line_number++;
+		execute_opcode(buffer, line_num, &top);
+		line_num++;
 	}
 
 	free(buffer);
 	free_stack(top);
 
-	close(fd);
+	close(file);
 	return (0);
 }
