5de8e53fa1d62183e6f90f08795688938d05ad07
Sana Assim <sanaassim1@gmail.com>
2023-06-21 21:38:24 -0700

    Create div function

---
+++ div.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * div - Divide the second top element of the stack by the top element
+ *
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the divide instruction
+ *
+ * Return: void
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top;
+
+	if (stack && *stack && (*stack)->prev)
+	{
+		top = stack_top(stack, line_number);
+		if (top->n == 0)
+		{
+			fprintf(stderr, "L%u: division by zero\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		(*stack)->n = (*stack)->n / top->n;
+		free(top);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
