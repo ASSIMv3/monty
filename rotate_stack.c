bca154f272c6e2ed62887bc8064b4e9e9b4c2fc9
ASSIMv3 <hassanassim01@gmail.com>
2023-06-22 02:09:32 -0700

    Create rotate right function

--- rotate_stack.c
+++ rotate_stack.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * rotate - Rotates the stack to the top, moving the top element to the bottom
+ * rotate_l - Rotates the stack to the top, moving the top element to the bottom
  *
  * @stack: Double pointer to the top of the stack
  * @line_number: Line number where the opcode is encountered
  *
  * Return: void
  */
-void rotate(stack_t **stack,
+void rotate_l(stack_t **stack,
 		 __attribute__((unused)) unsigned int line_number)
 {
 	if (stack && *stack && (*stack)->prev)
@@ -30,4 +30,35 @@ void rotate(stack_t **stack,
 		second_top->next = NULL;
 		*stack = second_top;
 	}
+}
+
+/**
+ * rotate_r -  rotates the stack to the bottom
+ *
+ * @stack: Double pointer to the stack
+ * @line_number: Line number of the instruction
+ *
+ * Return: void
+ */
+void rotate_r(stack_t **stack,
+		__attribute__((unused))unsigned int line_number)
+{
+	stack_t *last;
+
+	if (!stack || !(*stack) || !(*stack)->prev)
+		return;
+
+	/* get last element */
+	last = *stack;
+	while (last && last->prev)
+		last = last->prev;
+
+	/* rotate */
+	last->next->prev = NULL;
+	last->prev = *stack;
+	last->next = NULL;
+	(*stack)->next = last;
+
+	/* change TOS */
+	*stack = last;
 }
