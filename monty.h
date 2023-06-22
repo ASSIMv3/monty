d7645bfc49edd171a41a0f7fb6e3af76967e8f20
Sana Assim <sanaassim1@gmail.com>
2023-06-21 22:28:21 -0700

    Add modulo function prototype

--- monty.h
+++ monty.h
@@ -64,5 +64,6 @@ void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void division(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
+void modulo(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
